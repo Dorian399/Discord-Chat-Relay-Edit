257a2b43b9ed26f34565a4263bc4b18a9ec03aae
Dorian399 <92229312+Dorian399@users.noreply.github.com>
2024-08-28 18:09:55 +0200

    Update config.js

--- Bot - Nubs Discord Chat Relay/config.js
+++ Bot - Nubs Discord Chat Relay/config.js
@@ -2,14 +2,14 @@
 // This version of discord.js doesn't support the new Discord username system
 // Setting this to true injects my own code into discord.js to make names display properly.
 // If you're running a version newer than v14.11.0, you can likely set this to false
-exports.DiscordUsernameFix = true;
+exports.DiscordUsernameFix = false;
 
 // This is your Discord bot token, obtained from https://discord.com/developers/applications
 // To get your token, do the following:
 //     1. Follow the link and sign into Discord if not already signed in.
 //     2. Go to the "Bot" section and create a new bot account.
 //     3. You should see a button to copy the token. If not, click the regenerate button.
-exports.DiscordBotToken = "your_discord_bot_token";
+exports.DiscordBotToken = "your_discord_token";
 
 // This is the IP of the gmod server. You can get it by running `status` in the console.
 // Should be in format 0.0.0.0, you do not need :27145
@@ -26,7 +26,7 @@ exports.PortNumber = 42069;
 // Otherwise you can leave this empty.
 //     1. Sign into steam if not already.
 //     2. The domain can be anything. Fill that out and you can generate a key
-exports.SteamAPIKey = "";
+exports.SteamAPIKey = "your_steam_api_key";
 
 // If you're using a Steam API Key, how often should avatars be refreshed, in minutes? 
 // The lower the number, the more often you will actually be sending requests to Steam, which could get ratelimited and cause issues
@@ -45,6 +45,35 @@ exports.MaxMessageLength = 512;
 // This is the prefix for dev commands, see end of file
 exports.ManagerCommandPrefix = "--";
 
+// Enable --eval command. I'd recommend to leave it disabled, since it can be a big security risk.
+exports.EvalEnable = false;
+
+// Show errors when using the --eval command.
+
+exports.ShowEvalErrors = false;
+
+// Hide connections/close Websocket Status notifications.
+
+exports.HideWebsocketNotifs = false;
+
+// Hide join/leave messages.
+
+exports.HideJoinLeaveNotifs = false;
+
+// Hide "Bot started" message.
+
+exports.HideBotStartMessage = false;
+
+// Disables /status and /statusm commands.
+
+exports.DisableInteractions = false;
+
+
+// How often should the servers status get updated (in seconds) ?
+
+exports.StatusRefreshTime = 60;
+
+
 // Fill this with strings of Discord user IDs that you want to be people who can run developer 
 // commands on the bot. A string is a sequence of text wrapped in quotes, "like this." These values
 // should be separated by a comma. Example:
@@ -54,8 +83,31 @@ exports.ManagerCommandPrefix = "--";
 // ];
 // You will need to put your Discord ID into here in order to set up the bot.
 exports.Managers = [
-
 ];
+
+
+// Send commands using rcon, this allows using restricted commands as well as see there output. This needs the exports.ServerRcons to be setup properly.
+
+exports.UseRconForCommands = false;
+
+// This is the list of server ids to use with the rcon fallback.
+// Example:
+// exports.ServerRcons = {
+//     "server_id": {
+//         ip: "127.0.0.1",
+//         port: 27015,
+//         password: "password"
+//     },
+//     "server_id_2": {
+//         ip: "127.0.0.1",
+//         port: 27016,
+//         password: "password"
+//     }
+// };
+
+exports.ServerRcons = {
+};
+
 
 /*
 This bot also has a slash command, /status. This command will collect a list of all players on the server, connecting, in game, or AFK.
@@ -76,7 +128,12 @@ For all of these commands listed, you will start them with ManagerCommandPrefix 
     setgmodchannel 
         - Sets the relay channel. Messages sent in the gmod server will appear in this channel and vice versa.
         > Example: --setgmodchannel
-
+    setstatuschannel 
+        - Sets the channel with a permanent realtime status message
+        > Example: --setstatuschannel 
+	setstatusmchannel 
+        - Sets the channel with a permanent realtime compact status message
+        > Example: --setstatusmchannel 
     restart
     shutdown 
         - These two are aliases. Both will exit the node process. It'll only actually restart if you're running the .bat file or something else.
